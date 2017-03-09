95319af4b5a1c91cb815717977016c791d15aa9d
Delio <Delio@192.168.1.36>
2017-03-09 17:32:13 +0100

    Version 0.3v Metodo para saber la resolución de la pantalla

---
+++ src/maagic/interfaz/Dimensionpantalla.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package maagic.interfaz;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+/**
+ *
+ * @author Delio
+ */
+public class Dimensionpantalla {
+    public void devolverdimension(){
+    Toolkit tk=Toolkit.getDefaultToolkit();
+    Dimension tamaño=tk.getScreenSize();
+    
+    
+    }
+    
+}
