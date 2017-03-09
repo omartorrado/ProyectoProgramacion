01ce07aa61d6e2a2f1cd557dccc18dfb407cd17b
Delio <Delio@192.168.1.36>
2017-03-09 17:31:37 +0100

    Version 0.3v Cambios en Menu

--- src/maagic/interfaz/Menu.java
+++ src/maagic/interfaz/Menu.java
@@ -5,19 +5,27 @@
  */
 package maagic.interfaz;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 /**
  *
  * @author Delio
  */
 public class Menu extends javax.swing.JFrame {
-
+private Dimension dim;
     /**
      * Creates new form Menu2
      */
     public Menu() {
         initComponents();
+        Dimensionpantalla dimension =new Dimensionpantalla();
+        
+       
+      
     }
 
+      
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -27,38 +35,21 @@ public class Menu extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        Boton1Jugar = new javax.swing.JButton();
-        Boton2Baraja = new javax.swing.JButton();
-        Boton3Salir = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        Boton3Salir = new javax.swing.JButton();
+        Boton2Baraja = new javax.swing.JButton();
+        Boton1Jugar = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setUndecorated(true);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        Boton1Jugar.setBackground(new java.awt.Color(102, 140, 144));
-        Boton1Jugar.setFont(new java.awt.Font("Felix Titling", 0, 11)); // NOI18N
-        Boton1Jugar.setForeground(new java.awt.Color(102, 0, 102));
-        Boton1Jugar.setText("Jugar");
-        Boton1Jugar.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        Boton1Jugar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                Boton1JugarActionPerformed(evt);
-            }
-        });
-        getContentPane().add(Boton1Jugar, new org.netbeans.lib.awtextra.AbsoluteConstraints(693, 360, 620, 60));
+        jLabel2.setIcon(new javax.swing.ImageIcon("C:\\Users\\Delio\\Documents\\NetBeansProjects\\ProyectoProgramacion\\src\\maagic\\interfaz\\serien_grosses_logo_239-Recuperado.png")); // NOI18N
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(500, 20, -1, 290));
 
-        Boton2Baraja.setBackground(new java.awt.Color(102, 140, 144));
-        Boton2Baraja.setFont(new java.awt.Font("Felix Titling", 0, 11)); // NOI18N
-        Boton2Baraja.setForeground(new java.awt.Color(0, 102, 102));
-        Boton2Baraja.setText("Crear Baraja");
-        Boton2Baraja.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        Boton2Baraja.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                Boton2BarajaActionPerformed(evt);
-            }
-        });
-        getContentPane().add(Boton2Baraja, new org.netbeans.lib.awtextra.AbsoluteConstraints(780, 470, 440, 50));
+        jPanel1.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
 
         Boton3Salir.setBackground(new java.awt.Color(102, 140, 144));
         Boton3Salir.setFont(new java.awt.Font("Felix Titling", 0, 11)); // NOI18N
@@ -70,13 +61,61 @@ public class Menu extends javax.swing.JFrame {
                 Boton3SalirActionPerformed(evt);
             }
         });
-        getContentPane().add(Boton3Salir, new org.netbeans.lib.awtextra.AbsoluteConstraints(900, 570, 200, 50));
 
-        jLabel2.setIcon(new javax.swing.ImageIcon("C:\\Users\\Delio\\Documents\\NetBeansProjects\\ProyectoProgramacion\\src\\maagic\\interfaz\\serien_grosses_logo_239-Recuperado.png")); // NOI18N
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(500, 20, -1, 290));
+        Boton2Baraja.setBackground(new java.awt.Color(102, 140, 144));
+        Boton2Baraja.setFont(new java.awt.Font("Felix Titling", 0, 11)); // NOI18N
+        Boton2Baraja.setForeground(new java.awt.Color(0, 102, 102));
+        Boton2Baraja.setText("Crear Baraja");
+        Boton2Baraja.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        Boton2Baraja.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                Boton2BarajaActionPerformed(evt);
+            }
+        });
+
+        Boton1Jugar.setBackground(new java.awt.Color(102, 140, 144));
+        Boton1Jugar.setFont(new java.awt.Font("Felix Titling", 0, 11)); // NOI18N
+        Boton1Jugar.setForeground(new java.awt.Color(102, 0, 102));
+        Boton1Jugar.setText("Jugar");
+        Boton1Jugar.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        Boton1Jugar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                Boton1JugarActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(60, 60, 60)
+                        .addComponent(Boton2Baraja, javax.swing.GroupLayout.PREFERRED_SIZE, 501, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(26, 26, 26)
+                        .addComponent(Boton1Jugar, javax.swing.GroupLayout.PREFERRED_SIZE, 575, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(200, 200, 200)
+                        .addComponent(Boton3Salir, javax.swing.GroupLayout.PREFERRED_SIZE, 212, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(29, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap(32, Short.MAX_VALUE)
+                .addComponent(Boton1Jugar, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(28, 28, 28)
+                .addComponent(Boton2Baraja, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(29, 29, 29)
+                .addComponent(Boton3Salir, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(710, 300, 630, 230));
 
         jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\Delio\\Documents\\NetBeansProjects\\ProyectoProgramacion\\src\\maagic\\interfaz\\The-dragon-of-hell-liam-a-dragon-37509157-1920-1080.jpg")); // NOI18N
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1920, 1080));
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1930, 1080));
 
         pack();
         setLocationRelativeTo(null);
@@ -93,7 +132,7 @@ public class Menu extends javax.swing.JFrame {
     }//GEN-LAST:event_Boton2BarajaActionPerformed
 
     private void Boton3SalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Boton3SalirActionPerformed
-        System.exit(0);
+       
     }//GEN-LAST:event_Boton3SalirActionPerformed
 
     /**
@@ -138,5 +177,6 @@ public class Menu extends javax.swing.JFrame {
     private javax.swing.JButton Boton3Salir;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
