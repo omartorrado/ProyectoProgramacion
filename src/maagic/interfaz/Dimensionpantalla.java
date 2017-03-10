cb574eeff6d415e13021e58ca9bf2c74b2812d64
otorradomiguez <otorradomiguez@nautilus8.danielcastelao.org>
2017-03-10 09:54:15 +0100

    Creados metodos en DimensionPantalla para ajustar el tamaño y la posicion de un JComponent en base a la resolucion de pantalla

--- src/maagic/interfaz/Dimensionpantalla.java
+++ src/maagic/interfaz/Dimensionpantalla.java
@@ -6,18 +6,54 @@
 package maagic.interfaz;
 
 import java.awt.Dimension;
+import java.awt.Point;
 import java.awt.Toolkit;
+import javax.swing.JComponent;
 
 /**
  *
  * @author Delio
  */
 public class Dimensionpantalla {
-    public void devolverdimension(){
+    
+    //Metodo para ajustar el tamaño del componente a la pantalla, manteniendo
+    //La proporcion de tamaño original
+    public static void resizeComponente(JComponent j){
     Toolkit tk=Toolkit.getDefaultToolkit();
     Dimension tamaño=tk.getScreenSize();
+    Dimension tamañoNuevo=new Dimension(((int)((j.getWidth()/j.getRootPane().getWidth())*tamaño.getWidth())),((int)((j.getHeight()/j.getRootPane().getHeight())*tamaño.getHeight())));
+    j.setSize(tamañoNuevo);
+    /*
+    //Ancho del componente
+    int jW=j.getWidth();
+    //Alto del componente
+    int jH=j.getHeight();
+    //Ancho del panel padre
+    int pW=j.getRootPane().getWidth();
+    //Alto del panel padre
+    int pH=j.getRootPane().getHeight();
+    //Formula generica
+    int anchoNuevo=(int)((jW/pW)*tamaño.getWidth());
+    int altoNuevo=(int)((jH/pH)*tamaño.getHeight());
+    //Todo esto lo meto directamente en el constructor de tamañoNuevo
+    */    
+    }
     
+    //Metodo para recolocar el componente adapatandose al cambio de resolucion
+    public static void recolocarComponente(JComponent j){
+        Toolkit tk=Toolkit.getDefaultToolkit();
+        Dimension tamaño=tk.getScreenSize();
+        float proporcionH=tamaño.width/j.getRootPane().getWidth();
+        float proporcionV=tamaño.height/j.getRootPane().getHeight();
+        Point posicionNueva=new Point((int)(j.getLocation().x*proporcionH),(int)(j.getLocation().y*proporcionV));
+        j.setLocation(posicionNueva);
+                
+    }
     
+    //Meto ambos metodos en uno por comodidad. Los dejo escritos por separado por claridad
+    public static void adaptarResolucion(JComponent j){
+        resizeComponente(j);
+        recolocarComponente(j);
     }
     
 }
